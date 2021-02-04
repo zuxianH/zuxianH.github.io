639a679ba1b3f4ed42320c9986643b281aee2b0a
Superposition71 <zuxian135zh@gmail.com>
2021-02-04 21:26:51 +0100

    Site updated: 2021-02-04 21:26:50

---
+++ js/chart.js
@@ -0,0 +1,4 @@
+// 通过jsDelivr的CDN引入echarts
+<script src="https://cdn.jsdelivr.net/npm/echarts@4.8.0/dist/echarts.min.js"></script>
+// 使用GL里的各种组件时需要添加，否则可不需要
+<script src="https://cdn.jsdelivr.net/npm/echarts-gl@1.1.1/dist/echarts-gl.min.js"></script>
